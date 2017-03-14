f4a4f71bc616994a133fcc2ca23fc2a5c80ae776
Remy-hom <remylim8@gmail.com>
2017-03-15 12:37:18 +1300

    - Extract interfaces - Separate classes

---
+++ refactor-me/Api/IProductOptionService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using refactor_me.Models;
+
+namespace refactor_me.Api
+{
+    interface IProductOptionService
+    {
+        ProductOption GetOption(Guid productId, Guid id);
+
+        ProductOptions GetOptions(Guid productId);
+
+        void CreateOption(Guid id, ProductOption option);
+
+        void UpdateOption(Guid id, ProductOption option);
+
+        void DeleteOption(Guid id);
+    }
+}
